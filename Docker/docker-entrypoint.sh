4c5aec0dad9766b21da3f914a48d3b9a0362dc89
kelvinpaska <k.paska@amsterdam.nl>
2018-07-16 14:12:41 +0200

    updated code

--- Docker/docker-entrypoint.sh
+++ Docker/docker-entrypoint.sh
@@ -6,16 +6,16 @@ set -e
 
 cat > /app/app/config/parameters.yml <<EOF
 parameters:
-  mailer_transport: ${MM_DASHBOARD__MAILER__TRANSPORT}
-  mailer_host: ${MM_DASHBOARD__MAILER__HOST}
-  mailer_user: ${MM_DASHBOARD__MAILER__USER}
-  mailer_password: ${MM_DASHBOARD__MAILER__PASSWORD}
-  mailer_port: ${MM_DASHBOARD__MAILER__PORT}
-  mailer_encryption: ${MM_DASHBOARD__MAILER__ENCRYPTION}
-  secret: ${MM_DASHBOARD__SECRET}
-  markt_api.url: ${MM_DASHBOARD__API_URL}
+  mailer_transport:     ${MM_DASHBOARD__MAILER__TRANSPORT}
+  mailer_host:          ${MM_DASHBOARD__MAILER__HOST}
+  mailer_user:          ${MM_DASHBOARD__MAILER__USER}
+  mailer_password:      ${MM_DASHBOARD__MAILER__PASSWORD}
+  mailer_port:          ${MM_DASHBOARD__MAILER__PORT}
+  mailer_encryption:    ${MM_DASHBOARD__MAILER__ENCRYPTION}
+  secret:               ${MM_DASHBOARD__SECRET}
+  markt_api.url:        ${MM_DASHBOARD__API_URL}
   markt_api.guzzle.verify: true
-  mm_app_key: ${MM_DASHBOARD__APP_KEY}
+  mm_app_key:           ${MM_DASHBOARD__APP_KEY}
   session_cookie_only_secure_connection: true
 EOF
 
